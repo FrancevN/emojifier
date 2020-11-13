44e65f0fefa0e3d731b8e7d26ed936491eafff7f
Nikita <56350701+FrancevN@users.noreply.github.com>
2020-11-13 17:25:20 +0300

    Add files via upload

---
+++ emojifier.py
@@ -0,0 +1,26 @@
+emoji = {"a": "😀", "b": "😉", "c": "😂", "d": "🍪", "e": "😍", "f": "🤣", "g": "😊", "h": "🙏", "i": "💕", "j": "🤷",
+         "k": "🔥", "l": "😭", "m": "😘", "n": "👍", "o": "🥰", "p": "😎", "q": "😆", "r": "😁", "s": "🤔", "t": "😜",
+         "u": "🌟", "v": "💖", "w": "🪐", "x": "🌹", "y": "🎉", "z": "✨"}
+
+
+def demojify(emoji_text):
+    res1 = ''
+    for i in emoji_text:
+        for key, value in emoji.items():
+            if i == value:
+                res1 += key
+    return res1
+
+
+def emojify(text):
+    res = ''
+    for y in text:
+        res += emoji.get(y)
+    return res
+
+
+print(demojify('😜😍🤔😜'))
+print(emojify('test'))
+# stroka = input()
+# for em in stroka:
+#     print(kek(em), end="")
--- test_two.py
+++ test_two.py
@@ -1,8 +1,14 @@
-from emojifier import kek
-from emojifier_inverted import kek1
+from emojifier import emojify, demojify
+import pytest
 
 
-def test_emojifier():
-    emoji = kek(input())
-    emoji_inverted = kek1(emoji)
-    assert emoji == emoji_inverted
+@pytest.mark.parametrize('_input', ['test', 'a'])
+def test_demojifier(_input):
+    res = demojify(emojify(_input))
+    assert _input == res
+
+
+@pytest.mark.parametrize('_input', [('a', "😀"), ['test', '😜😍🤔😜']])
+def test_emojifier(_input):
+    res = emojify(_input[0])
+    assert _input[1] == res
